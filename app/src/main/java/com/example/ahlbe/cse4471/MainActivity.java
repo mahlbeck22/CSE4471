0d4a37cb311d84801d03e76845259eaa39c109bd
Michael Ahlbeck <Michael Ahlbeck>
2017-03-04 15:24:34 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ahlbe/cse4471/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ahlbe.cse4471;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
