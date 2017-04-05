74d71da05805e5fcbe907efae800a7cfbc1ec957
Kevin Payravi <kevinpayravi@gmail.com>
2017-04-04 23:41:52 -0400

    Fixing button targets + layout centering

--- app/src/main/java/com/example/ahlbe/cse4471/Decryption.java
+++ app/src/main/java/com/example/ahlbe/cse4471/Decryption.java
@@ -2,22 +2,16 @@ package com.example.ahlbe.cse4471;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-
-import static com.example.ahlbe.cse4471.R.id.button4;
-import static com.example.ahlbe.cse4471.R.id.button5;
 
 public class Decryption extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_decryption);
+        setContentView(R.layout.activity_encryption);
         final Button button4 = (Button) findViewById(R.id.button4);
         button4.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
--- app/src/main/java/com/example/ahlbe/cse4471/Encryption.java
+++ app/src/main/java/com/example/ahlbe/cse4471/Encryption.java
@@ -2,10 +2,7 @@ package com.example.ahlbe.cse4471;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,7 +11,7 @@ public class Encryption extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_encryption);
+        setContentView(R.layout.activity_decryption);
         final Button button7 = (Button) findViewById(R.id.button7);
         button7.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
